efe9e74b2325741a6680345a73be6a223a179519
alexanderholzinger <xandlholzi@gmail.com>
2023-04-19 11:05:32 +0200

    fix permission

--- app/src/main/java/com/example/vi_i__aufgabe_unfallbericht/MainActivity.java
+++ app/src/main/java/com/example/vi_i__aufgabe_unfallbericht/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.vi_i__aufgabe_unfallbericht;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.PackageManagerCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,10 +19,13 @@ import android.widget.ListView;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
+import java.io.PrintWriter;
 import java.util.LinkedList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,10 +37,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 134);
+        Log.d("TAG", "onCreate: "+ checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE));
+        try {
+            FileOutputStream count = openFileOutput("count.txt", MODE_PRIVATE);
+            PrintWriter pw = new PrintWriter(count);
+            pw.write(fileList().length-1 + "");
+            pw.close();
+            count.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         ap = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         lv = findViewById(R.id.listView);
         lv.setAdapter(ap);
         ll = new LinkedList<>();
+
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -64,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void intoListView() throws IOException, ClassNotFoundException {
+
 
         BufferedReader br = new BufferedReader(new InputStreamReader(openFileInput("count.txt")));
         int currantPositon = Integer.parseInt(br.readLine());
